9046cb1ffb2fb107c70b922d5a3ea47f32da6a04
John Allen <jallen79@jhu.edu>
2015-08-25 17:02:46 -0400

    day2homeprob1 code

---
+++ day2/day2homework-problem1.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+annotation = "/Users/cmdb/qbb2015/genomes/BDGP6.Ensembl.81.gtf"
+
+df = pd.read_table(annotation, comment = '#', header =None)
+
+df.columns = ["Chromosome", "database", "type", "start", "end", "score", "strand", "frame", "attributes"]
+
+#roi = df["Chromosome"].str.contains("2L")
+#print df[roi].shape
+
+#plt.figure()
+#plt.plot(df[roi]["start"])
+#plt.savefig("starts2L.png")
+
+#roi = df["Chromosome"].str.contains("2R")
+
+#plt.figure()
+#plt.plot(df[roi]["start"])
+#plt.xlabel("gene")
+#plt.savefig("starts2R.png")
+
+#for chromosome in ("2L", "2R", "Y"):
+    #roi = df["Chromosome"].str.contains(chromosome)
+    #plt.figure()
+    #plt.plot(df[roi]["start"])
+    #plt.title(chromosome)
+    #plt.ylabel("start position")
+    #plt.xlabel("gene")
+    #plt.savefig("starts" + chromosome + ".png")
+    
+subset = df["attributes"].str.contains("Sxl")
+plt.figure()
+plt.plot(df[subset]["start"])
+plt.title("Sxl")
+plt.ylabel("start position")
+plt.xlabel("gene")
+plt.savefig("Sxl")
