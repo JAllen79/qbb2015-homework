c00310594d00ed3aa70b7bbe9f89e8ac7498210e
John Allen <jallen79@jhu.edu>
2015-08-25 11:08:46 -0400

    filter.py -- filter tRNA lines

---
+++ day2/filter.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+filename = "/Users/cmdb/qbb2015/stringtie/SRR072893/t_data.ctab"
+
+f = open(filename)
+
+for line in f:
+    fields = line.split()
+    if "tRNA" in fields[9]:
+        print line,
