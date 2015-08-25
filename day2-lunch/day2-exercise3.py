9ff8cea9b3b59cb9956b23f0c1d3b8cc233c3a02
John Allen <jallen79@jhu.edu>
2015-08-25 14:03:23 -0400

    Exercise 3 Answers

---
+++ day2-lunch/day2-exercise3.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+filename = "/Users/cmdb/qbb2015/genomes/mappedreads.sam"
+
+f = open(filename)
+#line_count = 0
+#for line in f:
+#    line_count += 1
+    
+    
+#print "This is Question 1"
+#print line_count - 1
+
+
+
+perfect_matches = 0
+for line in (f):
+    if "NH:i:1" in line:
+        perfect_matches += 1
+    else: 
+        pass
+            
+print perfect_matches
