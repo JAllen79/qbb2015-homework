70c874dc6ba878b9489fdd679b1065ab58020465
John Allen <jallen79@jhu.edu>
2015-08-26 14:57:25 -0400

    problem 3

---
+++ day3/day3lunch_prob3.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv("/Users/cmdb/qbb2015/stringtie/SRR072893/t_data.ctab", sep="\t")
+
+FPKM_values = []
+
+
+for value in df["FPKM"]:
+    if value > 0:
+        FPKM_values.append(np.log(value))
+        
+FPKM_values = pd.Series(np.random.randn(1000))        
+FPKM_values.plot(kind='kde')
+plt.savefig("day3lunch_problem3.png")
+    
+#print FPKM_values
+#plt.hist(FPKM_values)
+#plt.savefig("day3lunch_problem2.png")
+#plt.figure()
+#plt.bar(range(len(chromosomes)), chromosomes.values())
+#plt.xticks(range(len(chromosomes)), chromosomes.values())
+#plt.savefig("Barplot.png")
+
+#Sxl = []
+
+#for sample in metadata[metadata["sex"] == "female"]["sample"]:
+#    df = pd.read_table("~/qbb2015/stringtie/" + sample + "/t_data.ctab")
+ #   roi = df["t_name"].str.contains("FBtr0331261")
+  #  df[roi]["FPKM"].values
+   # Sxl.append(df[roi]["FPKM"].values)
